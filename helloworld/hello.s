be3a83e8cc218040752038c980f3cedaee33dc74
koekeishiya <aasvi93@hotmail.com>
2017-07-08 14:26:13 +0200

    _exit -> syscall

--- helloworld/hello.s
+++ helloworld/hello.s
@@ -8,18 +8,16 @@ _hello:
 _phello:
     pushq %rbp
     movq %rsp, %rbp
-
     movb $1, %al
     movl $42, %esi
     movq _hello@GOTPCREL(%rip), %rdi
     callq _printf
-    xorq %rax, %rax
-
     popq %rbp
-    ret
+    retq
 
 _main:
     subq $8, %rsp
     callq _phello
-    movq $0, %rdi
-    callq _exit
+    movq $0x2000001, %rax
+    xorq %rdi, %rdi
+    syscall
